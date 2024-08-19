78a9fc8ecf7e74a0d76a153cb865838968eee661
KARTHICKJAYASANKAR <karthickj452@gmail.com>
2024-08-19 09:56:21 +0530

    webpack changed

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,33 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const {ModuleFederationPlugin} = require("webpack").container;
+const { ModuleFederationPlugin } = require("webpack").container;
 const path = require("path");
 
 const htmlPlugin = new HtmlWebPackPlugin({
   template: "./public/index.html",
   filename: "./index.html"
 });
+
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV || 'development', // Use 'development' as default but allow production mode
   devServer: {
     static: path.join(__dirname, "dist"),
     port: 3001,
-    historyApiFallback:{
-      index:'/public/index.html'
-    },
+    historyApiFallback: true, // This simplifies handling of history API in dev mode
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/', // Ensure files are served correctly
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader"
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
       }
-    }
     ]
   },
   plugins: [
@@ -31,10 +36,8 @@ module.exports = {
       name: "MicroFrontend",
       filename: "remoteEntry.js",
       exposes: {
-        "./Button":"./src/Button"
+        "./Button": "./src/Button"
       }
     })
   ]
 };
-
-// Checkout: "Checkout@http://localhost:3000/remoteEntry.js"
